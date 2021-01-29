2daf30a31dbe7c1605c74d21e12d4c7671fdeb5b
Hiago Coelho <b8coelho@gmail.com>
2021-01-29 16:02:06 -0300

    fix: instancia do firebase no chat

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,11 +1,37 @@
 import React, { useState } from 'react'
 
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDgJ2owlGYZGH0STvjeBYmmaBWZ_lPk-9Q",
+    authDomain: "tellme-app-e0def.firebaseapp.com",
+    projectId: "tellme-app-e0def",
+    storageBucket: "tellme-app-e0def.appspot.com",
+    messagingSenderId: "23172765752",
+    appId: "1:23172765752:web:5fe1d9c15759c55cd52ecd",
+    measurementId: "G-5E25B0B3Y7"
+  };
+
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
+
 export default function Chat () {
     const [formValue, setFormValue] = useState('')
 
+    
     const sendMessage = async(e) => {
         e.preventDefault()
-        console.log(formValue)
+        if(formValue !== '') {
+            console.log(formValue)
+        } else {
+            return
+        }
     }
 
     return (
