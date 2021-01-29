4907b4dfcb48ac3212be6ed2304600c186a1c022
Hiago Coelho <b8coelho@gmail.com>
2021-01-29 16:44:32 -0300

    fix: removidas configuraçoes do firebase

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,35 +1,7 @@
 import React, { useState } from 'react'
 
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
-import 'firebase/analytics'
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-var firebaseConfig = {
-    apiKey: "AIzaSyChTP3z-EsR49PLiM5V6U30tOdxGdx6xc4",
-    authDomain: "tellme-89144.firebaseapp.com",
-    projectId: "tellme-89144",
-    storageBucket: "tellme-89144.appspot.com",
-    messagingSenderId: "491322420817",
-    appId: "1:491322420817:web:0bf975a401b6ee709e1270",
-    measurementId: "G-SHNMPJQ5WR"
-}
-
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig)
-    const auth = firebase.auth()
-    const firestore = firebase.firestore()
-    const analytics = firebase.analytics()
- } else {
-    firebase.app()
- }
-
 export default function Chat () {
     const [formValue, setFormValue] = useState('')
-
     
     const sendMessage = async(e) => {
         e.preventDefault()
