2d8fc0cdf9b1cb07453f377539b1e2d8df2ff670
Hiago Coelho <b8coelho@gmail.com>
2021-02-01 15:32:49 -0300

    feat: componente identifica se a mensagem foi enviada ou recebida pelo usuario

--- components/ChatMessage.jsx
+++ components/ChatMessage.jsx
@@ -1,5 +1,10 @@
 export default function ChatMessage (props) {
     const { text, uid } = props.message;
+    const messageClass = uid === props.currentUserUid ? 'sent' : 'received';
 
-    return <p>{text}</p>
+    return (
+        <div className={`message ${messageClass}`}>
+            <p>{text}</p>
+        </div>
+    )
 }
